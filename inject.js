414bbe3c8ed0e133705ca92343aa6a799c616f81
Zac Connelly <zconnelly13@gmail.com>
2015-04-14 23:11:22 -0700

    mvp commit! displays table at bottom after mouse-over

--- inject.js
+++ inject.js
@@ -1,42 +1,157 @@
-// GLOBALS
-/*
- * Dict of 
- * key = infoBox's x-coordinate on transform (I'm not sure what this is, tbh)
- * value = 
- * {
- *  'distance': 0.0, // miles
- *  'elevation': 0.0, // feet
- *  'grade': 0%, // percent
- *  'pace': {
- *    'min': 0, // minutes
- *    'sec': 0, // seconds
- *    'time': 0, // in minutes, but includes seconds, e.g. 6minutes 30seconds would be 6.5
- *  }
- *}
-*/
-var paceData = {} 
+// GLOBALS & Constants
+// first entry of paceData is manual (because strava does not provide this one)
+var paceData = {
+  // key is the time as a float (number of minutes)
+  '0' : {
+    'distance': 0.0, // miles
+    'time': {
+      'hour': 0,
+      'min': 0,
+      'sec': 0,
+      'time': 0.0, // float for number of minutes
+    },
+    'pace': {
+      'min': 0,
+      'sec': 0,
+      'time': 0.0, // float for number of minutes
+    }
+  }
+} 
+
+var DISTANCE_BOX_ID = 'infobox-text-distance';
+var PACE_BOX_ID = 'infobox-text-pace';
+var TIME_BOX_ID = 'crossBar';
+var ELEVATION_CHART_ID = 'elevation-chart';
+var VIEW_ID = "view";
 
 // Functions
 function gatherData() {
-    var infoBox = document.getElementById('infoBox');
-    // this is the x-coordinate on the infoBox's "transform" attribute
-    // I'm not really sure what it means
-    // But for each run they range from 12-639 inclusive
-    var transformX = extractTransformX(infoBox);
+    var distance = extractDistance();
+    var pace = extractPace();
+    var time = extractTime();
+    var key = time.time; 
+    var val = {
+      'distance': distance,
+      'time': time,
+      'pace': pace
+    }
+    paceData[key] = val;
 }
 
-function extractTransformX(infoBox) {
-  var transform = infoBox.getAttribute('transform');
-  var startClip = 11; // number of characters in "transform("
-  var endClip = transform.indexOf(',');
-  var transformX = transform.substring(startClip,endClip);
-  console.log(endClip);
+function extractDistance() {
+  var distance = document.getElementById(DISTANCE_BOX_ID).children[1].innerHTML;
+  distance = distance.replace(/[^0-9\.]/gi,'')
+  return parseFloat(distance);
+}
+
+function extractPace() {
+  var pace = document.getElementById(PACE_BOX_ID).children[1].innerHTML;
+  pace = pace.replace(/[^0-9:]/gi,'')
+  var paceMinutes = parseFloat(pace.substr(0,pace.indexOf(':')))
+  var paceSeconds = parseFloat(pace.substr(pace.indexOf(':')+1))
+  var paceTime = paceMinutes + (paceSeconds/60)
+  pace = {
+    'min' : paceMinutes,
+    'sec' : paceSeconds,
+    'time': paceTime
+  }
+  return pace
+}
+
+function extractTime() {
+  var time = document.getElementById(TIME_BOX_ID).children[1].innerHTML;
+  time = time.replace(/[^0-9:]/gi,'')
+  var numberOfColons = (time.match(/:/g) || []).length
+  // time is in seconds
+  if (numberOfColons == 0) {
+    var timeHours = 0;
+    var timeMinutes = 0;
+    var timeSeconds = parseFloat(pace);
+    var timeTime = (time/60);
+  } else if (numberOfColons == 1) {
+    var timeHours = 0;
+    var timeMinutes = parseFloat(time.substring(0,time.indexOf(':')))
+    var timeSeconds = parseFloat(time.substr(time.indexOf(':')+1))
+    var timeTime = timeMinutes + (timeSeconds/60)
+  } else if (numberOfColons == 2) {
+    var timeHours = parseFloat(time.substring(0,time.indexOf(':')))
+    var timeMinutes = parseFloat(time.substring(time.indexOf(':')+1,time.lastIndexOf(':')))
+    var timeSeconds = parseFloat(time.substr(time.lastIndexOf(':')+1))
+    var timeTime = (timeHours*60) + timeMinutes + (timeSeconds/60)
+  }
+  time = {
+    'hour': timeHours,
+    'min' : timeMinutes,
+    'sec' : timeSeconds,
+    'time': parseFloat(timeTime)
+  }
+  return time
+}
+
+function displaySplits() {
+  var splits = calculateSplits(paceData);
+  var table = document.createElement('table');
+  table.width = "150px";
+  table.className = "split-times";
+  for (var i=1;i<splits.length;i++) { 
+    var split = splits[i];
+    var row = document.createElement('tr');
+    row.innerHTML = "<td>" + split.distance + "</td>" + "<td> - </td>" + "<td>" + split.splitTimeMinutesSeconds + "</td>";
+    table.appendChild(row);
+  }
+  try {
+    var elements = document.getElementsByClassName('split-times')
+    for (var i=0;i<elements.length;i++) {
+      elements[i].style.display = 'none';
+    }
+  } catch(e) {
+    // pass
+  }
+  var wrapper = document.createElement('div');
+  wrapper.width = '150px';
+  wrapper.style.width = '150px';
+  wrapper.appendChild(table);
+  document.getElementById(VIEW_ID).appendChild(wrapper); 
+}
+
+function calculateSplits(data) {
+  var interval = 0.5;
+  var splitPoints = {};
+  for (point in data) {
+    var point = data[point];
+    if (point.distance % interval == 0 && (splitPoints[point.distance] === undefined || splitPoints[point.distance] > point.time.time)) {
+      splitPoints[point.distance] = point.time.time;
+    }
+  }
+  pointList = []
+  for (point in splitPoints) {
+    var time = splitPoints[point];
+    pointList.push({'distance': parseFloat(point), 'time': time});
+  }
+  pointList = pointList.sort(function(a,b) {return (a.distance - b.distance);});
+  for (var i=1;i<pointList.length;i++) {
+    var point = pointList[i];
+    var previous = pointList[i-1];
+    var splitTime = point.time - previous.time;
+    splitTimeMinutes = parseInt(Math.floor(splitTime));
+    splitTimeSeconds = parseInt(Math.floor((splitTime-Math.floor(splitTime))*60))
+    if (splitTimeSeconds < 10) {
+      splitTimeSeconds = "0" + splitTimeSeconds
+    }
+    splitTimeMinutesSeconds = splitTimeMinutes + ":" + splitTimeSeconds
+    pointList[i]['splitTime'] = splitTime;
+    pointList[i]['splitTimeMinutesSeconds'] = splitTimeMinutesSeconds;
+  }
+  return pointList;
 }
 
 function setEventListeners() {
-  var chart = document.getElementById('elevation-chart')
+  var chart = document.getElementById(ELEVATION_CHART_ID)
   chart.addEventListener('mousemove',function() {
     gatherData();
+  });
+  chart.addEventListener('mouseout',function() {
+    displaySplits();
   });
 }
 
